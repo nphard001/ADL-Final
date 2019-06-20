f4c123a7804dc5ff5ab9ee1d3d913a608af28aab
nphard001 <nphard001@gmail.com>
2019-06-20 15:47:25 +0800

    ADD: UserProfile, user_text_view

--- chatbot/host_data/models.py
+++ chatbot/host_data/models.py
@@ -6,8 +6,41 @@ class AttrMetadata(BaseModel):
     img_path = models.TextField(default='None')
     dsr_path = models.TextField(default='None')
 
+class UserProfile(BaseModel):
+    updated = models.DateTimeField(auto_now=True, db_index=True)
+    dialog_end = models.DateTimeField(default=timezone.now, db_index=True)
+    username = models.TextField(default='None', db_index=True)
+    line_userId = models.TextField(default='None', db_index=True)
+    mode = models.TextField(default='auto', db_index=True) # auto / debug
+
 class UserEvent(BaseModel):
-    created = models.DateTimeField(auto_now_add=True)
-    line_userId = models.TextField(default='None')
-    line_replyToken = models.TextField(default='None')
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    state = models.TextField(default='pending', db_index=True) # pending / done
+    line_userId = models.TextField(default='None', db_index=True)
+    line_replyToken = models.TextField(default='None', db_index=True)
+    line_timestamp = models.IntegerField(default=-1, db_index=True)
+    line_type = models.TextField(default='None', db_index=True)
     line_event = models.TextField(default='None')
+    
+    def _get_line_text(self):
+        try:
+            evd = json.loads(self.line_event)
+            return evd['message']['text']
+        except KeyError:
+            return None
+    line_text = property(_get_line_text)
+    @classmethod
+    def from_event_dict(cls, event_dict: dict):
+        lineID = event_dict['source']['userId']
+        obj = cls(
+            line_userId = lineID,
+            line_replyToken = event_dict['replyToken'],
+            line_timestamp = event_dict['timestamp'],
+            line_type = event_dict['type'],
+            line_event = json.dumps(event_dict),
+        )
+        if UserProfile.objects.filter(line_userId=lineID).count()==0:
+            UserProfile(line_userId=lineID).save()
+        return obj
+
+
--- chatbot/host_data/views_data2line.py
+++ chatbot/host_data/views_data2line.py
@@ -1,38 +1,53 @@
 from host_data.views import *
-from host_data.models import UserEvent
+from host_data.models import UserEvent, UserProfile
 def _ApplyURLPatterns():
     urlpatterns.append(path(r'line_event', line_event_view))
+    urlpatterns.append(path(r'user_text', user_text_view))
+    urlpatterns.append(path(r'polling', polling_view))
 # ================================================================
 @csrf_exempt
 def line_event_view(request):
     json_body = json.loads(request.body.decode('utf-8'))
-    event = json_body['event_repr']
+    event_dict = json.loads(json_body['event_repr'])
+    
     print('--------==== line_event_view ====--------')
-    obj = UserEvent(
-        # line_userId = json_body['event']['source']['userId'],
-        # line_replyToken = json_body['event']['replyToken'],
-        line_event = json_body['event_repr']
-        )
+    print(json.dumps(event_dict, indent=1))
+    if event_dict['source']['type'] != 'user':
+        print('not user event, ignored')
+        return HttpResponse()
+    obj = UserEvent.from_event_dict(event_dict)
     obj.save()
-    print(json.dumps(json_body, indent=1))
+    print('UserEvent object:')
+    print(json.dumps(obj.to_dict(), indent=1))
     return HttpResponse()
-r'''
-{
- "message": {
-  "id": "10069058180662",
-  "packageId": "7920494",
-  "stickerId": "195012613",
-  "type": "sticker"
- },
- "replyToken": "b2ea723f79244cc59478d6bcd96f6bbf",
- "source": {
-  "type": "user",
-  "userId": "U9d6503da59b6b0d5efba3c7af7af5125"
- },
- "timestamp": 1560944438172,
- "type": "message"
-}
 
-'''
+@csrf_exempt
+def user_text_view(request):
+    json_body = json.loads(request.body.decode('utf-8'))
+    object_set = UserEvent.objects
+    object_set = object_set.filter(line_type='message')
+    if 'username' in json_body:
+        line_userId = UserProfile.objects.filter(username=json_body['username'])
+        print(line_userId)
+        if line_userId.count()>0:
+            line_userId = line_userId[0].line_userId
+            object_set = object_set.filter(line_userId=line_userId)
+        else:
+            object_set = object_set.filter(pk=-1)
+    if 'line_userId' in json_body:
+        object_set = object_set.filter(line_userId=json_body['line_userId'])
+    text_list = []
+    token = 'XD'
+    for evt_object in object_set.order_by('created'):
+        if evt_object.state=='pending':
+            token = evt_object.line_replyToken
+        txt = evt_object.line_text
+        if type(txt) == type('msg'):
+            text_list.append(txt)
+    return JsonResponse({
+        'state': 'done',
+        'token': token,
+        'text_list': text_list
+    }, safe=False)
 # ================================================================
 _ApplyURLPatterns()
